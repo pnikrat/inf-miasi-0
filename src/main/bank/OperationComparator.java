87b2caab2bf59ecfcb753f6b6cf1649b6ed59c5c
pnikrat <sniffoza@gmail.com>
2016-11-23 19:31:04 +0100

    Tests for BankOperations being filled in once Operations are done on BankProducts'

--- src/main/bank/OperationComparator.java
+++ src/main/bank/OperationComparator.java
@@ -6,4 +6,5 @@ public class OperationComparator implements Comparator<IOperation> {
     public int compare(IOperation o1, IOperation o2) {
         return o1.getExecutionDate().compareTo(o2.getExecutionDate());
     }
+    // TODO: Add several tests if operations are sorted chronologically
 }
--- src/test/bank/BankTest.java
+++ src/test/bank/BankTest.java
@@ -14,19 +14,25 @@ import static org.junit.Assert.*;
 public class BankTest {
 
     private Bank tester;
+    private Account accountForOperationTesting;
 
     @Before
     public void setUp() throws Exception {
         tester = new Bank();
         tester.createAccount("1234", 1);
+        accountForOperationTesting = (Account) tester.BankProducts.get(0);
+
         tester.createCredit((Account) tester.BankProducts.get(0),
                 new BigDecimal("1500.00").setScale(2, BigDecimal.ROUND_HALF_UP), "CRED001",
                 new BigDecimal("3.5").setScale(1, BigDecimal.ROUND_HALF_UP));
         tester.createTermDeposit((Account) tester.BankProducts.get(0),
                                 new BigDecimal("1000.00").setScale(2, BigDecimal.ROUND_HALF_UP),
                                 LocalDate.now(), "LOC001");
+
+        BigDecimal tempDepoAmount = new BigDecimal("5000.00").setScale(2, BigDecimal.ROUND_HALF_UP);
+        Deposit tempDepo = new Deposit(accountForOperationTesting, tempDepoAmount);
     }
-    //        TODO: Create tests for BankOperations being filled after Products' operations are made
+
     @Test
     public void testBankProductsContainsNewAccount() throws Exception {
         assertTrue(tester.BankProducts.stream().filter(x -> x.getProductNumber().equals("1234")).findFirst().isPresent());
@@ -42,4 +48,21 @@ public class BankTest {
         assertTrue(tester.BankProducts.stream().filter(x -> x.getProductNumber().equals("CRED001")).findFirst().isPresent());
     }
 
+    @Test
+    public void testBankOperationsContainsDepositOperationOnAccount() throws Exception {
+        assertTrue(tester.BankOperations.get(accountForOperationTesting).stream().filter(x -> x.getOperationTypeId()
+            .equals(1)).findFirst().isPresent());
+    }
+
+    @Test
+    public void testBankOperationsContainsCreateTermDepositOperationOnAccount() throws Exception {
+        assertTrue(tester.BankOperations.get(accountForOperationTesting).stream().filter(x -> x.getOperationTypeId()
+            .equals(4)).findFirst().isPresent());
+    }
+
+    @Test
+    public void testBankOperationsContainsCreateCreditOperationOnAccount() throws Exception {
+        assertTrue(tester.BankOperations.get(accountForOperationTesting).stream().filter(x -> x.getOperationTypeId()
+            .equals(6)).findFirst().isPresent());
+    }
 }
